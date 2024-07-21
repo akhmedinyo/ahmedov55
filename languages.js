4ed47b96c286ceaf7855ff86640a13bfd645041e
akhmedinyo <110104020+akhmedinyo@users.noreply.github.com>
2024-07-21 19:48:08 +0300

    Add files via upload

---
+++ languages.js
@@ -0,0 +1,32 @@
+const languages = [
+  {
+    no: "16",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+  {
+    no: "5",
+    name: "Azerbaijani",
+    native: "Azərbaycan dili",
+    code: "az",
+  },
+  {
+    no: "45",
+    name: "Russian",
+    native: "Русский",
+    code: "ru",
+  },
+  {
+    no: "53",
+    name: "Turkish",
+    native: "Türkçe",
+    code: "tr",
+  },
+  {
+    no: "54",
+    name: "Ukrainian",
+    native: "Українська",
+    code: "uk",
+  },
+];
